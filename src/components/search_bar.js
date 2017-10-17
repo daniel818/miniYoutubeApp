bd749e915005eac3e9d9ded1bd438c81bc8baec6
daniel818 <avital.daniel@gmail.com>
2017-10-17 03:32:29 +0300

    learning react + need more css

---
+++ src/components/search_bar.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Daniel on 19/06/2017.
+ */
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {term: ''};
+    }
+
+
+    render(){
+        return(
+            <div className="search-bar">
+                <input
+                    value={this.state.term}
+                    onChange={event => this.onInputChange(event.target.value)}/>
+
+            </div>
+        );
+    }
+    onInputChange(term){
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
+
+}
+
+export default SearchBar;
